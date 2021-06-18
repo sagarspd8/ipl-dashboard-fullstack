cb2cd50090ef14e0e5eb19c2277326be0df21ee7
Sagar Prasad <sagarspd8@gmail.com>
2021-06-19 01:52:36 +0530

    initial commit

---
+++ src/main/java/com/ilp/ilpdashboardfullstack/IlpDashboardFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.ilp.ilpdashboardfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlpDashboardFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlpDashboardFullstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ilp/ilpdashboardfullstack/IlpDashboardFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilp.ilpdashboardfullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlpDashboardFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
